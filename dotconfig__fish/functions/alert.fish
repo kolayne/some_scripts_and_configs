961346af07922f3a74fa2a65f72ee4108f814705
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2024-03-10 00:20:21 +0300

    Improve alert.fish: depend on exit code

--- dotconfig__fish/functions/alert.fish
+++ dotconfig__fish/functions/alert.fish
@@ -1,4 +1,10 @@
 function alert --description 'alias alert paplay /usr/share/sounds/Yaru/stereo/complete.oga & notify-send Completed (status current-commandline)'
-  paplay /usr/share/sounds/Yaru/stereo/complete.oga & notify-send Completed (status current-commandline)
+  if test "$status" -eq 0
+    paplay /usr/share/sounds/freedesktop/stereo/complete.oga &
+    notify-send Success (status current-commandline)
+  else
+    paplay /usr/share/sounds/Yaru/stereo/battery-low.oga &
+    notify-send Failed (status current-commandline)
+  end
         
 end
