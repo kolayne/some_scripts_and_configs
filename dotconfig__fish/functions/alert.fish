bc6ab87a3bfef51dfec1fffd8534a6816cf4383e
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2024-03-13 13:06:54 +0300

    alert.fish: keep exit code of the previous command

--- dotconfig__fish/functions/alert.fish
+++ dotconfig__fish/functions/alert.fish
@@ -1,10 +1,14 @@
-function alert --description 'alias alert paplay /usr/share/sounds/Yaru/stereo/complete.oga & notify-send Completed (status current-commandline)'
-  if test "$status" -eq 0
+function alert --description 'Play sound and send notification when the previous command completes. Return with the $status of the previous command'
+  set -l STATUS "$status"
+
+  if test "$STATUS" -eq 0
     paplay /usr/share/sounds/freedesktop/stereo/complete.oga &
     notify-send Success (status current-commandline)
   else
     paplay /usr/share/sounds/Yaru/stereo/battery-low.oga &
     notify-send Failed (status current-commandline)
   end
+
+  return "$STATUS"
         
 end
