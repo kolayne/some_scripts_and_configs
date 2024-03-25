48bc760edb16a1422c94360184c1e36d2033ddc6
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2024-03-25 22:26:13 +0300

    Several changes to config.fish
    - Remove udmount, add umnt, add uumnt
    
    - Add `show_run_time` function to show how long a command took as `fish_postexec`

--- dotconfig__fish/config.fish
+++ dotconfig__fish/config.fish
@@ -7,7 +7,40 @@ if status is-interactive
     abbr --add vpna wg-quick up amsterdam
     abbr --add vpnad wg-quick down amsterdam
 
-    abbr --add udmount udisksctl mount -b
+    abbr --add --set-cursor=% umnt udisksctl mount -b /dev/sd%
+    abbr --add --set-cursor=% uumnt udisksctl unmount -b /dev/sd%
 
     abbr --add inhib --set-cursor=% systemd-inhibit --what=%handle-lid-switch
+
+    function show_run_time --on-event fish_postexec
+        if test "$CMD_DURATION" -lt 5000
+            return
+        end
+
+        set --local millis "$CMD_DURATION"
+        set --local secs "$(math -s 0 $millis / 1000)"
+        set --local mins "$(math -s 0 $secs / 60)"
+        set --local hours "$(math -s 0 $mins / 60)"
+        set --local days "$(math -s 0 $hours / 24)"
+
+        echo -ne "\\033[90mRan in "
+        if test "$days" -gt 0
+            echo -n "{$days}d"
+        end
+        if test "$hours" -gt 0
+            echo -n "$(math -s 0 "$hours" % 24)h"
+        end
+        if test "$mins" -gt 0
+            echo -n "$(math -s 0 "$mins" % 60)m"
+        end
+        if test "$secs" -gt 0
+            echo -n "$(math -s 0 "$secs" % 60)s"
+        end
+        if test "$millis" -gt 0
+            echo -n "$(math -s 0 "$millis" % 1000)ms"
+        end
+
+        # Carriage return
+        echo
+    end
 end
