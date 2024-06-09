d58e7d7f9123945f229ee93131800b16cbd3abff
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2024-06-09 17:20:52 +0300

    Miscellaneous config updates
    - Re-enabled auto-start of some thing in i3
    - Added `tiling_drag` in i3 (both modifier key drag and titlebar drag
      are available)
    - Improved the `alert` fish function, added a new sound

--- dotconfig__fish/functions/alert.fish
+++ dotconfig__fish/functions/alert.fish
@@ -1,14 +1,14 @@
 function alert --description 'Play sound and send notification when the previous command completes. Return with the $status of the previous command'
-  set -l STATUS "$status"
+    set -l STATUS "$status"
 
-  if test "$STATUS" -eq 0
-    paplay /usr/share/sounds/freedesktop/stereo/complete.oga &
-    notify-send Success (status current-commandline)
-  else
-    paplay /usr/share/sounds/Yaru/stereo/battery-low.oga &
-    notify-send Failed (status current-commandline)
-  end
+    if test "$STATUS" -eq 0
+        paplay /usr/share/sounds/freedesktop/stereo/complete.oga & disown
+        notify-send Success (status current-commandline)
+    else
+        paplay ~/.config/fish/oof.mp3 & disown
+        notify-send Failed (status current-commandline)
+    end
 
-  return "$STATUS"
-        
+    return "$STATUS"
+
 end
