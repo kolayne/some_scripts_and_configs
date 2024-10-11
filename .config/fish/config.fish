9180272b06437c374746ab2393f6cf29555103a4
Nikolai Nechaev <nikolay_nechaev@mail.ru>
2024-10-11 21:53:44 +0300

    config.fish: `gcl` should clone recursively

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     abbr --add '``' --position anywhere --set-cursor=% '(%)'
 
     abbr --add gco git checkout
-    abbr --add --set-cursor=% gcl git clone git@github.com:%
+    abbr --add --set-cursor=% gcl git clone --recursive git@github.com:%
 
     abbr --add vpna wg-quick up amsterdam
     abbr --add vpnad wg-quick down amsterdam
